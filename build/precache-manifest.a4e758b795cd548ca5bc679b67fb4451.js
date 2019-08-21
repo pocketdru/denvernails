2fadb818c3de1bdd6d42d1c3b6bd49f3d712434e
pocketdru <pocketdru@gmail.com>
2019-08-21 17:18:12 -0600

    build added

---
+++ build/precache-manifest.a4e758b795cd548ca5bc679b67fb4451.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c81bebc36d866a350d2a726210d8f573",
+    "url": "/index.html"
+  },
+  {
+    "revision": "555dda1ab1d103325685",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "cfd18f068e965cd020ab",
+    "url": "/static/js/2.7dcb13d6.chunk.js"
+  },
+  {
+    "revision": "555dda1ab1d103325685",
+    "url": "/static/js/main.4acc5715.chunk.js"
+  },
+  {
+    "revision": "789654a7c778786b1218",
+    "url": "/static/js/runtime~main.4d42cd8b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
