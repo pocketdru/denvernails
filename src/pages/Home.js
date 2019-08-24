4609fec9660b8a00e218acb873fbb18823cc179e
pocketdru <pocketdru@gmail.com>
2019-08-23 19:02:22 -0600

    jumbotron created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
-import Navbar from "../components/Navs";
 import "./Home.css";
-
+import Navbar from "../components/Navs";
+import Jumbotron from "../components/Jumbotron";
 
 function Home() {
     return (
         <div className="container">
             <Navbar/>
+            <Jumbotron/>
         </div>
     )
 }
