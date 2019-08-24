1340495b056764b8dbd0ac37618b5b7b90f988ea
pocketdru <pocketdru@gmail.com>
2019-08-23 20:01:31 -0600

    jumbotron styled

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -2,7 +2,18 @@ import React from "react";
 import "./style.css";
 
 function Jumbotron() {
-
+    return (
+        <div className="row jumbotron-kudri">
+            <div className="col-12">
+            <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                    <h1 className="display-4">Fluid jumbotron</h1>
+                    <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                </div>
+            </div>
+            </div>
+        </div>
+    )
 }
 
 export default Jumbotron;
