7054495fdce6065ff3da4d9f2756da8578e554f9
pocketdru <pocketdru@gmail.com>
2019-09-02 17:45:07 -0600

    iframe styled

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,7 +6,6 @@ function Contacts() {
        <div className="row contact-kudri">
            <div className="col-5">
            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3071.814030017009!2d-104.9704926846271!3d39.65389967946106!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c809be0920fbd%3A0x9670ad62217ea22!2s1610%20E%20Girard%20Pl%20%23104%2C%20Englewood%2C%20CO%2080113!5e0!3m2!1sen!2sus!4v1567467203443!5m2!1sen!2sus" >
-           {/* width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" */}
            </iframe>
            </div>
        </div>
