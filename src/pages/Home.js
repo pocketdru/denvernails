65a0e4a02ad7fccb41f1fd9c3ce059359f093a2e
pocketdru <pocketdru@gmail.com>
2019-09-02 16:41:42 -0600

    contact section created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import "./Home.css";
 import Navbar from "../components/Navs";
 import Jumbotron from "../components/Jumbotron";
+import Contacts from "../components/Contact";
 
 function Home() {
     return (
         <div className="container">
             <Navbar/>
             <Jumbotron/>
+            <Contacts/>
         </div>
     )
 }
