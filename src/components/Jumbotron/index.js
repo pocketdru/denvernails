7dc90dac1eb6f1a180a24462fac1600026b9d449
pocketdru <pocketdru@gmail.com>
2019-09-01 19:55:52 -0600

    icons added and imported, and added to media list, kudri-button moved, jumbotron modified

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./style.css";
-import jumbotronMyPh from './jumbotron-my-ph1.png';
+import JumbotronMyPh from './jumbotron-my-ph1.png';
+import Comb from "./comb.png";
+import EyeShadow from "./eye-shadow.png";
+import EyeLashes from "./eyelashes.png";
+import HairDye from "./hair-dye.png";
+import NailPolish from "./nail-polish.png";
+
 
 function Jumbotron() {
     return (
@@ -11,18 +17,55 @@ function Jumbotron() {
                     <h2 className="text-center">My services</h2>
                     <div className="row justify-content-center">
                         <div className="col-4 kudri-list">
-                            <ul className="list-group">
+                            {/* <ul className="list-group">
                                 <li className="list-group-item">Hair and color</li>
                                 <li className="list-group-item">Nails</li>
                                 <li className="list-group-item">Lashes</li>
                                 <li className="list-group-item">Mens haircut</li>
                                 <li className="list-group-item">Makeup</li>
+                            </ul> */}
+                            <ul className="list-unstyled align-middle">
+                            <li className="media mb-2">
+                                <img src={HairDye} className="mr-3" alt="hair dye"/>
+                                <div className="media-body">
+                                <h5 className="mt-0 mb-1">Hair and color</h5>
+                                </div>
+                            </li>
+                            <li className="media mb-2">
+                                <img src={NailPolish} className="mr-3" alt="nail polish"/>
+                                <div className="media-body">
+                                <h5 className="mt-0 mb-1">Nails</h5>
+                                </div>
+                            </li>
+                            <li className="media mb-2">
+                                <img src={EyeLashes} className="mr-3" alt="eye lashes"/>
+                                <div className="media-body">
+                                <h5 className="mt-0 mb-1">Lashes</h5>
+                                </div>
+                            </li>
+                            <li className="media mb-2">
+                                <img src={Comb} className="mr-3" alt="comb"/>
+                                <div className="media-body">
+                                <h5 className="mt-0 mb-1">Mens haircut</h5>
+                                </div>
+                            </li>
+                            <li className="media mb-2">
+                                <img src={EyeShadow} className="mr-3" alt="eye shadow"/>
+                                <div className="media-body">
+                                <h5 className="mt-0 mb-1">Makeup</h5>
+                                </div>
+                            </li>
                             </ul>
+                            <div className="row kudri-button justify-content-center">
+                        <div className="col-2 mt-4 mb-2">
+                            <button type="button" className="btn btn-secondary btn-lg">View Portfolio</button>
+                        </div>
+                    </div>
                         </div>
                         <div className="col-6 kudri-list">
                             <ul className="list-group">
                                 <li className="list-group-item">
-                                    <img src={jumbotronMyPh} className="img-fluid mx-auto d-block" alt="Responsive image"/>
+                                    <img src={JumbotronMyPh} className="img-fluid mx-auto d-block" alt="Responsive image"/>
                                 </li>
                                 {/* <li className="list-group-item">Vestibulum at eros</li> */}
                             </ul>
@@ -30,11 +73,11 @@ function Jumbotron() {
                     </div>
                     
                     {/* <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p> */}
-                    <div className="row kudri-button justify-content-center">
-                    <div className="col-2 mt-4 mb-2">
-                        <button type="button" className="btn btn-secondary btn-lg">View Portfolio</button>
-                    </div>
-                </div>
+                    {/* <div className="row kudri-button justify-content-center">
+                        <div className="col-2 mt-4 mb-2">
+                            <button type="button" className="btn btn-secondary btn-lg">View Portfolio</button>
+                        </div>
+                    </div> */}
                 </div>
                 
             </div>
