2a532acb36d7685855b8b064a14ffd5932ba4148
pocketdru <pocketdru@gmail.com>
2019-09-04 16:16:34 -0600

    contact list filled, map margins fixed

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -11,10 +11,10 @@ function Contacts() {
            <div className="container">
                <div className="row">
                 <div className="col-5">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3071.814030017009!2d-104.9704926846271!3d39.65389967946106!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c809be0920fbd%3A0x9670ad62217ea22!2s1610%20E%20Girard%20Pl%20%23104%2C%20Englewood%2C%20CO%2080113!5e0!3m2!1sen!2sus!4v1567467203443!5m2!1sen!2sus" className="mt-5 mb-5" >
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3071.814030017009!2d-104.9704926846271!3d39.65389967946106!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c809be0920fbd%3A0x9670ad62217ea22!2s1610%20E%20Girard%20Pl%20%23104%2C%20Englewood%2C%20CO%2080113!5e0!3m2!1sen!2sus!4v1567467203443!5m2!1sen!2sus" className="mt-4 mb-4" >
                     </iframe>
                 </div>
-                <div className="col-4">
+                <div className="col-4 contact list">
                     <ul class="list-unstyled">
                         <li class="media">
                             <img src={Phone} class="mr-3" alt="Phone"/>
@@ -26,13 +26,28 @@ function Contacts() {
                         <li class="media">
                             <img src={Position} class="mr-3" alt="Location"/>
                             <div class="media-body">
-                            <h5 class="mt-0 mb-1">List-based media object</h5>
+                            <h5 class="mt-0 mb-1">1610 E Girard Place
+                            <br/>
+                             Suite 104
+                             <br/>
+                             Cherry Hills 80113
+                              </h5>
                             </div>
                         </li>
                         <li class="media">
                             <img src={Instagram} class="mr-3" alt="Instagram"/>
                             <div class="media-body">
-                            <h5 class="mt-0 mb-1">List-based media object</h5>
+                            <h5 class="mt-0 mb-1">
+                                <a href="https://www.instagram.com/kudrisalondenver/">@kudrisalondenver</a>
+                            </h5>
+                            </div>
+                        </li>
+                        <li class="media">
+                            <img src={Facebook} class="mr-3" alt="facebook"/>
+                            <div class="media-body">
+                            <h5 class="mt-0 mb-1">
+                                <a href="https://www.facebook.com/KUDRI-SALON-DENVER-165630237706620/">KUDRI SALON DENVER</a>
+                            </h5>
                             </div>
                         </li>
                     </ul>
