b93a1190fe7cdc2ee0ec382ca9bd1c959aa488ad
pocketdru <pocketdru@gmail.com>
2019-09-06 15:52:10 -0600

    footer content aligned and styled

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,13 +4,13 @@ import "./style.css";
 function Footer() {
     return (
         <div className="row navbar-kudri footer-kudri justify-content-end">
-            <div className="col-1">
+            <div className="col-1 mt-4 mb-auto">
                 <h6 className="align-middle">&copy;</h6>
             </div>
-            <div className="col-8">
+            <div className="col-6 mt-4 mb-auto">
                 <div>Icons made by 
-                    <a href="https://www.flaticon.com/authors/freepik" title="Freepik" className="align-middle">Freepik</a> 
-                    from
+                    <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> 
+                    from 
                     <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
                 </div>
             </div>
