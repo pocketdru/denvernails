2d114c8ffce80cde3e56331df1802779b6cf7c84
pocketdru <pocketdru@gmail.com>
2019-09-07 16:00:38 -0600

    toggleClass got to work and added to list element

--- src/components/PortfolioGrid/index.js
+++ src/components/PortfolioGrid/index.js
@@ -19,7 +19,7 @@ class PortfolioGrid extends Component {
             <div className="col-4">
                 <div className="list-group" id="list-tab" role="tablist">
                 <a className="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Home</a>
-                <a className="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Profile</a>
+                <a className={this.state.notActive} onClick={() => this.toggleClass()} id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Profile</a>
                 <a className="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Messages</a>
                 <a className="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Settings</a>
                 </div>
