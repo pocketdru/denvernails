10d1a1658d3c6b022ab72020103be4504d8a935e
pocketdru <pocketdru@gmail.com>
2019-09-09 16:35:33 -0600

    all images added to a hair component and aligned, PortfolioGrid background added

--- src/components/Hair/index.js
+++ src/components/Hair/index.js
@@ -11,11 +11,47 @@ import img7 from "./hair7.png";
 function Hair() {
     return (
         <div className="row hair">
+        <div className="row mb-2">
             <div className="col-4">
                 <a href="./hair1.png">
                     <img src={img1}></img>
                 </a>
             </div>
+            <div className="col-4">
+                <a href="./hair2.png">
+                    <img src={img2}></img>
+                </a>
+            </div>
+            <div className="col-4">
+                <a href="./hair3.png">
+                    <img src={img3}></img>
+                </a>
+            </div>
+        </div>
+        <div className="row mb-2">
+            <div className="col-4">
+                <a href="./hair4.png">
+                    <img src={img4}></img>
+                </a>
+            </div>
+            <div className="col-4">
+                 <a href="./hair5.png">
+                    <img src={img5}></img>
+                </a>
+            </div>
+            <div className="col-4">
+                <a href="./hair6.png">
+                    <img src={img6}></img>
+                </a>
+            </div>
+        </div>
+        <div className="row mb-2">
+            <div className="col-4">
+                <a href="./hair7.png">
+                    <img src={img7}></img>
+                </a>
+            </div>
+        </div>
         </div>
     )
 }
