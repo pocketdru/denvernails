991057c73df71ec84e2afbd508bd08be6f00c9d4
pocketdru <pocketdru@gmail.com>
2019-09-09 16:21:14 -0600

    all images styled and imported

--- src/components/Hair/index.js
+++ src/components/Hair/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./style.css";
 import img1 from "./hair1.png";
+import img2 from "./hair2.png";
+import img3 from "./hair3.png";
+import img4 from "./hair4.png";
+import img5 from "./hair5.png";
+import img6 from "./hair6.png";
+import img7 from "./hair7.png";
 
 function Hair() {
     return (
