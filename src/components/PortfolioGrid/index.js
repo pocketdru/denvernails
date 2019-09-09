dbce888d25b2540f0fce48eee685c42c452fddeb
pocketdru <pocketdru@gmail.com>
2019-09-08 23:35:14 -0600

    all side bar classes and tab-content classes been changed to the actual meaningfull ones

--- src/components/PortfolioGrid/index.js
+++ src/components/PortfolioGrid/index.js
@@ -5,76 +5,76 @@ class PortfolioGrid extends Component {
     state =  {
 
         //side bar class states
-        homeClass: "list-group-item list-group-item-action home",
-        progileClass: "list-group-item list-group-item-action profile",
-        messagesClass: "list-group-item list-group-item-action message",
-        settingsClass: "list-group-item list-group-item-action settings",
+        hairClass: "list-group-item list-group-item-action hair",
+        nailsClass: "list-group-item list-group-item-action nails",
+        lashesClass: "list-group-item list-group-item-action lashes",
+        makeupClass: "list-group-item list-group-item-action makeup",
 
         //selected side's bar data states
-        dataHome: "tab-pane fade home",
-        dataProfile: "tab-pane fade profile",
-        dataMessages: "tab-pane fade messages",
-        dataSettings: "tab-pane fade settings"
+        dataHair: "tab-pane fade hair",
+        dataNails: "tab-pane fade nails",
+        dataLashes: "tab-pane fade lashes",
+        dataMakeup: "tab-pane fade makeup"
 
     }
 
     toggleClass(n) {
         this.setState({
-            homeClass: "list-group-item list-group-item-action home",
-            progileClass: "list-group-item list-group-item-action profile",
-            messagesClass: "list-group-item list-group-item-action message",
-            settingsClass: "list-group-item list-group-item-action settings",
-
-            dataHome: "tab-pane fade home",
-            dataProfile: "tab-pane fade profile",
-            dataMessages: "tab-pane fade messages",
-            dataSettings: "tab-pane fade settings"            
+            hairClass: "list-group-item list-group-item-action hair",
+            nailsClass: "list-group-item list-group-item-action nails",
+            lashesClass: "list-group-item list-group-item-action lashes",
+            makeupClass: "list-group-item list-group-item-action makeup",
+    
+            dataHair: "tab-pane fade hair",
+            dataNails: "tab-pane fade nails",
+            dataLashes: "tab-pane fade lashes",
+            dataMakeup: "tab-pane fade makeup"      
         })
 
         if (n === 1) {
-            let myClass = this.state.homeClass;
-            return myClass === "list-group-item list-group-item-action home" ? 
+            let myClass = this.state.hairClass;
+            return myClass === "list-group-item list-group-item-action hair" ? 
             this.setState({
-                homeClass: "list-group-item list-group-item-action home active",
-                dataHome: "tab-pane fade home show active"
+                hairClass: "list-group-item list-group-item-action hair active",
+                dataHair: "tab-pane fade hair show active"
 
             }) : 
             this.setState({
-                homeClass: "list-group-item list-group-item-action home",
-                dataHome: "tab-pane fade home"
+                hairClass: "list-group-item list-group-item-action hair",
+                dataHair: "tab-pane fade hair"
             }) 
         } else if (n === 2) {
-            let myClass = this.state.progileClass;
-            return myClass === "list-group-item list-group-item-action profile" ? 
+            let myClass = this.state.nailsClass;
+            return myClass === "list-group-item list-group-item-action nails" ? 
             this.setState({
-                progileClass: "list-group-item list-group-item-action profile active",
-                dataProfile: "tab-pane fade profile show active"
+                nailsClass: "list-group-item list-group-item-action nails active",
+                dataNails: "tab-pane fade nails show active"
             }) : 
             this.setState({
-                progileClass: "list-group-item list-group-item-action profile",
-                dataProfile: "tab-pane fade profile"
+                nailsClass: "list-group-item list-group-item-action nails",
+                dataNails: "tab-pane fade nails"
             }) 
         } else if (n === 3) {
-            let myClass = this.state.messagesClass;
-            return myClass === "list-group-item list-group-item-action message" ? 
+            let myClass = this.state.lashesClass;
+            return myClass === "list-group-item list-group-item-action lashes" ? 
             this.setState({
-                messagesClass: "list-group-item list-group-item-action message active",
-                dataMessages: "tab-pane fade messages show active"
+                lashesClass: "list-group-item list-group-item-action lashes active",
+                dataLashes: "tab-pane fade lashes show active"
             }) : 
             this.setState({
-                messagesClass: "list-group-item list-group-item-action message",
-                dataMessages: "tab-pane fade messages"
+                lashesClass: "list-group-item list-group-item-action lashes",
+                dataLashes: "tab-pane fade lashes"
             }) 
         } else if (n === 4) {
-            let myClass = this.state.settingsClass;
-            return myClass === "list-group-item list-group-item-action settings" ? 
+            let myClass = this.state.makeupClass;
+            return myClass === "list-group-item list-group-item-action makeup" ? 
             this.setState({
-                settingsClass: "list-group-item list-group-item-action settings active",
-                dataSettings: "tab-pane fade settings show active"
+                makeupClass: "list-group-item list-group-item-action makeup active",
+                dataMakeup: "tab-pane fade makeup show active"
             }) : 
             this.setState({
-                settingsClass: "list-group-item list-group-item-action settings",
-                dataSettings: "tab-pane fade settings"
+                makeupClass: "list-group-item list-group-item-action makeup",
+                dataMakeup: "tab-pane fade makeup"
             }) 
         }
         // return myClass === "list-group-item list-group-item-action" ? 
@@ -84,20 +84,20 @@ class PortfolioGrid extends Component {
     render () {
     return (
         <div className="row portfolio-grid">
-            <div className="col-4">
+            <div className="col-2">
                 <div className="list-group" id="list-tab" role="tablist">
-                <a className={this.state.homeClass} onClick={() => this.toggleClass(1)} id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Home</a>
-                <a className={this.state.progileClass} onClick={() => this.toggleClass(2)} id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Profile</a>
-                <a className={this.state.messagesClass} onClick={() => this.toggleClass(3)} id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Messages</a>
-                <a className={this.state.settingsClass} onClick={() => this.toggleClass(4)} id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Settings</a>
+                <a className={this.state.hairClass} onClick={() => this.toggleClass(1)} id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Home</a>
+                <a className={this.state.nailsClass} onClick={() => this.toggleClass(2)} id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Profile</a>
+                <a className={this.state.lashesClass} onClick={() => this.toggleClass(3)} id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Messages</a>
+                <a className={this.state.makeupClass} onClick={() => this.toggleClass(4)} id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Settings</a>
                 </div>
             </div>
-            <div className="col-8">
+            <div className="col-10">
                 <div className="tab-content" id="nav-tabContent">
-                <div className={this.state.dataHome} id="list-home" role="tabpanel" aria-labelledby="list-home-list">1</div>
-                <div className={this.state.dataProfile} id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">2</div>
-                <div className={this.state.dataMessages} id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">3</div>
-                <div className={this.state.dataSettings} id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">4</div>
+                <div className={this.state.dataHair} id="list-home" role="tabpanel" aria-labelledby="list-home-list">1</div>
+                <div className={this.state.dataNails} id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">2</div>
+                <div className={this.state.dataLashes} id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">3</div>
+                <div className={this.state.dataMakeup} id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">4</div>
                 </div>
             </div>
         </div>
