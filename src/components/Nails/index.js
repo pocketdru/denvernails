bba0cd4097f5434b58d259eaa05d31360da06002
pocketdru <pocketdru@gmail.com>
2019-09-12 14:39:48 -0600

    all cards created and images added

--- src/components/Nails/index.js
+++ src/components/Nails/index.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./style.css";
 import img1 from "./img/nail1.png";
-import img2 from "./img/nail1.png";
-import img3 from "./img/nail1.png";
-import img4 from "./img/nail1.png";
-import img5 from "./img/nail1.png";
-import img6 from "./img/nail1.png";
-import img7 from "./img/nail1.png";
+import img2 from "./img/nail2.png";
+import img3 from "./img/nail3.png";
+import img4 from "./img/nail4.png";
+import img5 from "./img/nail5.png";
+import img6 from "./img/nail6.png";
+import img7 from "./img/nail7.png";
+import img8 from "./img/nail8.png";
+import img9 from "./img/nail9.png";
+import img10 from "./img/nail10.png";
+import img11 from "./img/nail11.png";
+import img12 from "./img/nail12.png";
+import img13 from "./img/nail13.png";
+import img14 from "./img/nail14.png";
+import img15 from "./img/nail15.png";
+import img16 from "./img/nail16.png";
+import img17 from "./img/nail17.png";
 
 function Nails() {
     return (
@@ -36,8 +46,44 @@ function Nails() {
 </div>
 <div className="card-group mb-3">
   <div className="card ml-3">
-    <img src={img7} className="card-img-top" alt="haircut"/>
+        <img src={img7} className="card-img-top" alt="haircut"/>
     </div>
+    <div className="card ml-3">
+        <img src={img8} className="card-img-top" alt="haircut"/>
+    </div>
+    <div className="card ml-3">
+        <img src={img9} className="card-img-top" alt="haircut"/>
+    </div>
+</div>
+<div className="card-group mb-3">
+  <div className="card ml-3">
+    <img src={img10} className="card-img-top" alt="haircut"/>
+  </div>
+  <div className="card ml-3">
+    <img src={img11} className="card-img-top" alt="haircut"/>
+  </div>
+  <div className="card ml-3">
+    <img src={img12} className="card-img-top" alt="haircut"/>
+  </div>
+</div>
+<div className="card-group mb-3">
+  <div className="card ml-3">
+    <img src={img13} className="card-img-top" alt="haircut"/>
+  </div>
+  <div className="card ml-3">
+    <img src={img14} className="card-img-top" alt="haircut"/>
+  </div>
+  <div className="card ml-3">
+    <img src={img15} className="card-img-top" alt="haircut"/>
+  </div>
+</div>
+<div className="card-group mb-3">
+  <div className="card ml-3">
+    <img src={img16} className="card-img-top" alt="haircut"/>
+  </div>
+  <div className="card ml-3">
+    <img src={img17} className="card-img-top" alt="haircut"/>
+  </div>
 </div>
 </div>
     )
