8902e3d4eedeb3e4be7a70107b890a9660a108ea
pocketdru <pocketdru@gmail.com>
2019-09-16 15:24:51 -0600

    boostrap components added to review-kudri

--- src/components/Review/index.js
+++ src/components/Review/index.js
@@ -4,14 +4,14 @@ import "./style.css";
 function Review() {
     return (
         <div className="row review-kudri">
-            <div className="col-2">
-
-            </div>
-            <div className="col-2">
-                
-            </div>
-            <div className="col-2">
-                
+            <div className="col-12">
+            <ul className="list-group list-group-horizontal">
+                <li className="list-group-item">
+                    <img src="..." className="img-fluid" alt="Responsive image"/>
+                </li>
+                <li className="list-group-item">Dapibus ac facilisis in</li>
+                <li className="list-group-item">Morbi leo risus</li>
+            </ul>
             </div>
         </div>
     )
