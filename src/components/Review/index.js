4323f7859fa1f110c7e8a13ac399b6b8cd1ff9bd
pocketdru <pocketdru@gmail.com>
2019-09-16 15:13:46 -0600

    kudri-review grid added

---
+++ src/components/Review/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css";
+
+function Review() {
+    return (
+        <div className="row review-kudri">
+            <div className="col-2">
+
+            </div>
+            <div className="col-2">
+                
+            </div>
+            <div className="col-2">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Review;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./Home.css";
 import Navbar from "../components/Navs";
 import Jumbotron from "../components/Jumbotron";
+import Review from "../components/Review";
 import Contacts from "../components/Contact";
 import Footer from "../components/Footer";
 
@@ -10,6 +11,7 @@ function Home() {
         <div className="container">
             <Navbar/>
             <Jumbotron/>
+            <Review/>
             <Contacts/>
             <Footer/>
         </div>
